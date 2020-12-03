59b99219ab0a670f7f4f171e5976bb182cb87957
Julia Kim <26291536+julia-kim@users.noreply.github.com>
2020-12-02 22:41:14 -0500

    Update custom.js with custom CSS classes

--- src/main/resources/static/js/custom.js
+++ src/main/resources/static/js/custom.js
@@ -1,14 +1,14 @@
 $(document).ready(function () {
   $(".unfollow_btn").hover(
     function () {
-      $(this).removeClass("btn-primary")
-      $(this).addClass("btn-danger")
+      $(this).removeClass("btn-chirper")
+      $(this).addClass("btn-chirper-danger")
       $(this).html("Unfollow")
     },
     function () {
       $(this).html("Following")
-      $(this).removeClass("btn-danger")
-      $(this).addClass("btn-primary")
+      $(this).removeClass("btn-chirper-danger")
+      $(this).addClass("btn-chirper")
     }
   )
 })
